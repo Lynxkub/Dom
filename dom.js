5dfd6b9f56d83b5d1219a2de8c0576d6c8e3bfc3
Lynxkub <jek82@nau.edu>
2021-09-02 20:35:55 -0500

    adding

---
+++ dom.js
@@ -0,0 +1,31 @@
+// document.getElementById("container");
+
+// document.querySelector("#container");
+
+// document.querySelectorAll(".second");
+
+// document.querySelector("ol .third");
+
+// const container = document.querySelector("#container");
+//     container.innerText="Hello!";
+
+// const main=document.querySelector("#container");
+// main.classList.add("main");
+
+// let footer=document.querySelector(".footer");
+// footer.classList.remove("main");
+
+// const newLi=document.createElement("li");
+
+// newLi.innerText="four";
+
+// const ul=document.querySelector("ul");
+// ul.appendChild(newLi);
+
+// const olLis=document.querySelectorAll("ol li");
+// for (let lis of olLis){
+//     lis.style.backgroundColor="green"
+// }
+
+// const footer=document.querySelector(".footer");
+// footer.remove();
